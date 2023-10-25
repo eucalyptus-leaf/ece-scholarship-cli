5caf7688d29b541cd6e2b7976e48815e15ac50ba
gbjones3 <gbjones3@ncsu.edu>
2023-10-25 05:48:29 -0400

    adding data-structure code for tech-demo

--- dataStructure/scholarship.py
+++ dataStructure/scholarship.py
@@ -1,25 +1,81 @@
-# sholarship.py
+"""
+This module contains the Scholarship class which represents a scholarship.
+"""
+
+from linked_list import LinkedList
 
 class Scholarship:
-    def __init__(self, name, budget, numAwards, priority):
+    """
+    The Scholarship class represents a scholarship.
+    """
+    def __init__(self, scholarship_id, name, budget, num_awards, priority, criteria=None):
+        """
+        Initializes a new instance of the Scholarship class.
+
+        :param scholarship_id: A unique identifier for the scholarship.
+        :param name: The name of the scholarship.
+        :param budget: The budget of the scholarship.
+        :param num_awards: The number of awards offered by the scholarship.
+        :param priority: The priority of the scholarship.
+        """
+        self.scholarship_id = scholarship_id
         self.name = name
         self.budget = budget
-        self.numAwards = numAwards
+        self.num_awards = num_awards
         self.priority = priority
-        self.requirements = set()
+        self.criteria = set()
+        self.students = LinkedList()
 
-    def search_requirements(self, requirement):
-        for n in self.requirements:
-            if n in requirement:
-                return True
-        return False
+    def add_criteria(self, criteria):
+        """
+        Adds a criteria to the scholarship.
+
+        :param criteria: The criteria to add.
+        """
+        self.criteria.add(criteria)
+        
+
+    def remove_criteria(self, criteria):
+        """
+        Removes a criteria from the scholarship.
+
+        :param criteria: The criteria to remove.
+        """
+        self.criteria.discard(criteria)
+
+
+    def search_criteria(self, criteria):
+        """
+        Searches for a criteria in the scholarship's criteria.
+
+        :param criteria: The criteria to search for.
+        :return: True if the criteria is found, False otherwise.
+        """
+        return criteria in self.criteria
     
-    def add_requirement(self, requirement):
-        self.requirements.append(requirement)
-        return
-    
-    def delete_requirement(self, requirement):
-        if requirement in self.requirements:
-            self.requirements.discard(requirement)
-            return True
-        else: return False
+
+    def add_student(self, student_id):
+        """
+        Adds a student to the scholarship's ordered linked-list of students.
+
+        :param student_id: The unique identifier of the student to add.
+        """
+        self.students.insert_sorted(student_id)
+
+
+    def remove_student(self, student_id):
+        """
+        Removes student from the scholarship's ordered linked-list of students.
+
+        :param student_id: The unique identifier of the student applying.
+        """
+        self.students.remove(student_id)
+
+
+    def compare_students(self, student1, student2):
+        # This method should return a positive number if student1 is more qualified than student2,
+        # a negative number if student2 is more qualified than student1, and 0 if they are equally qualified.
+        # The criteria for comparison will depend on the specific criteria for the scholarship.
+        """ TODO: Implement a method for determing how the criterion match and how they compare """
+        
+        return student2.qualityscore - student1.qualityscore
--- dataStructure/student.py
+++ dataStructure/student.py
@@ -1,26 +1,67 @@
-# student.py
+"""
+This module contains the Student class which represents a student.
+"""
 
 class Student:
-    # Init variables that every student has but are different for each
-    def __init__(self, firstName, lastName, ID):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.ID = ID
+    """
+    The Student class represents a student.
+    """
+    def __init__(self, first_name, last_name, student_id):
+        """
+        Initializes a new instance of the Student class.
+
+        :param first_name: The first name of the student.
+        :param last_name: The last name of the student.
+        :param student_id: The unique identifier of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.student_id = student_id
         self.attributes = set()
+        self.applications = {}
 
-    # Student Methods
-    def search_attributes(self, attribute):
-        for n in self.attributes:
-            if n in attribute:
-                return True
-        return False
-    
     def add_attribute(self, attribute):
-        self.attributes.append(attribute)
-        return
+        """
+        Adds an attribute to the student.
 
-    def delete_attribute(self, attribute):
-        if attribute in self.attributes:
-            self.attributes.discard(attribute)
-            return True
-        else: return False
+        :param attribute: The attribute to add.
+        """
+        self.attributes.add(attribute)
+
+    def remove_attribute(self, attribute):
+        """
+        Removes an attribute from the student.
+
+        :param attribute: The attribute to remove.
+        """
+        self.attributes.discard(attribute)
+
+    def search_attributes(self, attribute):
+        """
+        Searches for an attribute in the student's attributes.
+
+        :param attribute: The attribute to search for.
+        :return: True if the attribute is found, False otherwise.
+        """
+        return attribute in self.attributes
+
+    def add_application(self, application):
+        """
+        Adds an application to the student's applications.
+
+        :param application: The application to add.
+        """
+        self.applications[application.scholarship_id] = application
+
+    def remove_application(self, scholarship_id):
+        """
+        Removes an application from the student's applications.
+
+        :param scholarship_id: The unique identifier of the scholarship being applied to.
+        """
+        if scholarship_id in self.applications:
+            del self.applications[scholarship_id]
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.student_id})'
+    
