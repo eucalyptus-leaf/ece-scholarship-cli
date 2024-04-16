4e9c1172442771c435d0568a2f502cbdf353bd24
lpbost <lpbost@ncsu.edu>
2024-04-16 19:22:10 -0400

    Update award_system.py

--- library/award_system.py
+++ library/award_system.py
@@ -2,15 +2,22 @@
 
 class AwardSystem:
     def __init__(self):
+        ''' Description: Initialize empty dictionary and list'''
         self.awarded = {}
         self.award_order = []
 
     def order_scholarships(self, scholarshipTab):
-        # Order scholarships by priority
+        ''' Description: Order scholarships by priority
+            Args: scholarshipTab
+        '''
         self.award_order = sorted(scholarshipTab, key=lambda x: -x.priority, reverse=True)
 
     def award_scholarships_loevan(self, h, studentTab, scholarshipTab, budgetSystem):
-        # Award scholarships to students based on Loevan's algorithm
+        ''' Description: Matching algorithm to award scholarhsips
+            Args: h, studentTab, scholarshipTab, budgetSystem
+            Returns: Awarded scholarships
+            Error State: Returns empty
+        '''
         for scholarship in self.award_order:
             if scholarship.scholarship_id not in self.awarded:
                 self.awarded[scholarship.scholarship_id] = {}
@@ -37,7 +44,11 @@ class AwardSystem:
                         print("Awarded " + str(student.awarded[scholarship.scholarship_id]) + " from scholarship " + str(scholarship.scholarship_id) + " to student " + str(student.student_id))
     
     def get_awards_string(self, scholarship_id):
-        # Print the students awarded a scholarship
+        ''' Description: Print the students awarded scholarships
+            Args: scholarship_id
+            Returns: Prints awarded scholarships
+            Error State: Print no students awarded
+        '''
         string = ""
         if scholarship_id in self.awarded:
             for student_id, amount in self.awarded[scholarship_id].items():
